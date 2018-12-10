0f6aec87fbf215f09ee005eea25153a69e1b428b
Man Cao <manc@google.com>
2018-12-10 16:26:16 +0800

    Add Eclipse C++ project settings.

---
+++ eclipse/create_jdkGenSrc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TARGET_DIR="$HOME"/workspace/jdkHEADgensrc
+JDK_BUILD_DIR="$HOME"/ws/jdkHeadOpenBuildFastdebug
+
+if [[ ! -d "$TARGET_DIR" ]]; then
+  mkdir -p "$TARGET_DIR"
+fi
+
+cp -r "$JDK_BUILD_DIR"/images/jdk/include/* "$TARGET_DIR"/
+cp -r "$JDK_BUILD_DIR"/hotspot/variant-server/gensrc/* "$TARGET_DIR"/
+
+find "$TARGET_DIR" -regextype posix-extended -regex ".*[.](cmdline|log)$" | xargs rm
+
+echo "Copied to $TARGET_DIR"
